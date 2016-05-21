d80d1a05757c74fb77843afac2442d905c73b951
shikhar-kr <shikhar.kr@gmail.com>
2016-05-21 19:06:16 +0300

    Update config.php

--- app/addons/shopify/config.php
+++ app/addons/shopify/config.php
@@ -7,9 +7,9 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 define('LOG_FILE', Tygh\Registry::get('config.dir.var') .'/shopify.log') ;
 
-define('API_LOGIN', 'ge@myst.uk.net');
-define('API_KEY', 'B7a69B9n154VKnp1oC36a7sLeOecOxs0');
+define('API_LOGIN', 'abc@example.com');
+define('API_KEY', 'B7a69B9n154VKsdere36a7sLeOecOxs0');
 
-define('SHOPIFY_DOMAIN', 'cscart-2.myshopify.com');
-define('SHOPIFY_API_KEY', '77cedb4ad0920a80de1583091b42ccd4');
-define('SHOPIFY_API_PASS', '0bdd3da62c4a8d3e3b0bdafaeddcd733');
+define('SHOPIFY_DOMAIN', 'myshopify.com');
+define('SHOPIFY_API_KEY', '77cedb4ada80de1583091b42ccd4');
+define('SHOPIFY_API_PASS', '0bdd3da6d3e3b0bdafaeddcd733');
